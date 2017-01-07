bf311ef52ada55f498a814e35fddb12db182c68b
Prarinya Ekapho <prarinya.e@gmail.com>
2017-01-07 20:10:24 +0700

    First uplaod to GitHub

---
+++ ws3/LCD_I2C_Serial_Workshop/LCD_I2C_Serial_Workshop.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>  // Comes with Arduino IDE
+#include <LiquidCrystal_I2C.h>
+
+//LiquidCrystal_I2C lcd(0x3F, 16,2);
+LiquidCrystal_I2C lcd(0x27, 16,2);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  lcd.init();
+  for(int i = 0; i<3; i++)
+  {
+    lcd.backlight();
+    delay(250);
+    lcd.noBacklight();
+    delay(250);
+  }
+  lcd.backlight();
+  
+  lcd.setCursor(0,0);
+  lcd.print(".........");
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print(".........");
+  delay(8000);
+ }
+
+void loop()
+{
+  {
+  // when characters arrive over the serial port...
+  if(Serial.available()){
+    // wait a bit for the entire message to arrrive
+    delay(300);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    while(Serial.available()>0)
+    {
+      // display each character to the LCD
+      lcd.write(Serial.read());
+    }
+   }
+  }
+}
+ 
---
+++ ws4/NodeMCU_Ctrl_NETPIE_Freeboard/NodeMCU_Ctrl_NETPIE_Freeboard.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h> 
+#include <MicroGear.h>
+
+const char* ssid = "itbakery-wifi"; 
+const char* password = "itbakery@9";
+
+#define APPID "KobSmartFarm" 
+#define KEY "SIpLYGJzlBD7ZRl" 
+#define SECRET "uZSvYG6oyoNdVeRiJNEFlDhzK"
+#define ALIAS "piectrl"
+WiFiClient client;
+char state = 0;
+char stateOutdated = 0; 
+char buff[16];
+
+MicroGear microgear(client);
+
+void sendState() {
+  if (state == 0)
+    microgear.publish("/piectrl/state","0");
+   else
+    microgear.publish("/piectrl/state","1");
+   Serial.println("send state...");
+   stateOutdated = 0;
+}
+
+void updateIO() {
+  if (state >= 1) {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else {
+    state = 0;
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+}
+
+void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
+      char m = *(char *)msg;
+      Serial.print("Incoming message --> ");
+      msg[msglen] = '\0';
+      Serial.println((char *)msg);
+
+      if (m == '0' || m == '1') {
+        state = m=='0'?0:1;
+        updateIO();
+      }
+      if (m == '0' || m == '1' || m == '?') stateOutdated = 1;
+}
+
+void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
+      Serial.println("Connected to NETPIE...");
+      microgear.setAlias(ALIAS);
+      stateOutdated = 1;
+}
+
+void setup() { 
+
+  Serial.begin(115200);
+  Serial.println("Starting...");
+
+  pinMode (LED_BUILTIN, OUTPUT);
+
+  if (WiFi.begin(ssid, password)) {
+      while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+      }
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  microgear.on(MESSAGE,onMsghandler);
+  microgear.on(CONNECTED,onConnected);
+  microgear.init(KEY,SECRET,ALIAS);
+  microgear.connect(APPID);
+}
+
+void loop() {
+  if (microgear.connected()) {
+    if (stateOutdated) sendState();
+    microgear.loop();
+  }
+  else { 
+    Serial.println("connection lost, reconnect...");
+      microgear.connect(APPID);
+  }
+}
+
